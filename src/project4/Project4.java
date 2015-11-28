77a3b06f78a56e9fe7186813348e401bd1b55ba3
stereotype13 <stereotype13@hotmail.com>
2015-11-28 01:22:17 -0800

    Added gson-2.5.jar

--- src/project4/Project4.java
+++ src/project4/Project4.java
@@ -1,9 +1,14 @@
 package project4;
 
 import studentScheduleApp.StudentScheduleApp;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Project4 {
 	public static void main(String[] args) {
+		Gson gson = new GsonBuilder().create();
+		gson.toJson("hello", System.out);
+		gson.toJson(123, System.out);
 		StudentScheduleApp theApp = new StudentScheduleApp(args);
 		theApp.run();
 	}
