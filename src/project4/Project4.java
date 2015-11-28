5ab47a53c47bde82824b5729dd1fb1759d5137e3
stereotype13 <stereotype13@hotmail.com>
2015-11-28 00:12:04 -0800

    First commit

---
+++ src/project4/Project4.java
@@ -0,0 +1,10 @@
+package project4;
+
+import studentScheduleApp.StudentScheduleApp;
+
+public class Project4 {
+	public static void main(String[] args) {
+		StudentScheduleApp theApp = new StudentScheduleApp(args);
+		theApp.run();
+	}
+}
